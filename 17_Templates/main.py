e28346912d96c261f1bbb68aa7be4efc1a05cdad
LightNow0 <kimlightnow@gmail.com>
2025-08-09 22:02:29 +0900

    17_End

--- 17_Templates/main.py
+++ 17_Templates/main.py
@@ -54,4 +54,3 @@ def read_macro(request: Request):
 def template_inherit(request: Request):
     my_text = "FastAPI와 Jinja2를 이용한 예시입니다."
     return templates.TemplateResponse("index.html", {"request": request, "text": my_text})
-
