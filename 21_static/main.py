22a6dc87c0e07cd15c2c6a6b507dfe0fe7206968
LightNow0 <kimlightnow@gmail.com>
2025-08-11 17:38:01 +0900

    21_static end

--- 21_static/main.py
+++ 21_static/main.py
@@ -1,6 +1,24 @@
+# from fastapi import FastAPI, Request
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.templating import Jinja2Templates
+
+# app = FastAPI()
+
+
+# # 정적 파일을 위한 설정
+# app.mount("/static", StaticFiles(directory='static'), name="static")
+
+# templates = Jinja2Templates(directory="templates")
+
+# # 메인페이지 라우트ㅡ
+# @app.get("/")
+# def read_root(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -11,6 +29,6 @@ app.mount("/static", StaticFiles(directory='static'), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # 메인페이지 라우트ㅡ
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
