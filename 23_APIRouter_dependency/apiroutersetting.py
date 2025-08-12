e90ae6d331d6d2e6c29daefcdc81f84bae9ef34b
LightNow0 <kimlightnow@gmail.com>
2025-08-13 01:34:55 +0900

    23_APIRouter_dependency  누락된 커밋사항과 24_Parameter 시작사항  등록

--- 23_APIRouter_dependency/apiroutersetting.py
+++ 23_APIRouter_dependency/apiroutersetting.py
@@ -34,3 +34,8 @@ parent_router.include_router(
 
 # 상위 라우터를 애플리케이션에 추가
 app.include_router(parent_router)
+
+
+# 상위 라우터인 parent_router에서 설정한 의존성 함수 common_dependency가
+# 하위 라우터인 child_router에서도 동작하는 것을 확인할 수 있습니다.
+# 위 코드를 실행했을 때 하위 라우터가 상위 라우터의 설정을 상속받아 사용되는 것을 볼 수 있습니다.
